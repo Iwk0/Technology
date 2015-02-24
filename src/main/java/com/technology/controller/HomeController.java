f8527ed918989acdc8e9d429d3ff03950be0c9d7
Iwk0 <ivomishev@gmail.com>
2015-02-24 17:02:42 +0200

    Update

--- src/main/java/com/technology/controller/HomeController.java
+++ src/main/java/com/technology/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
+    /*Logger*/
     private static final Logger logger = Logger.getLogger(HomeController.class);
 
     @Autowired
