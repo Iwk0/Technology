499816ae2396ab0634ec595c21d25de64fe64877
Iwk0 <ivomishev@gmail.com>
2015-03-26 18:33:07 +0200

    Update

--- src/main/java/com/technology/controller/HomeController.java
+++ src/main/java/com/technology/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.technology.controller;
 
 import com.technology.model.User;
-import com.technology.model.json.Rows;
-import com.technology.model.json.TableSettings;
+import com.technology.repository.CompanyRepository;
 import com.technology.repository.UserRepository;
+import com.technology.service.MainService;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,11 +23,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -40,6 +56,12 @@ public class HomeController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private MainService mainService;
+
 /*    @Value("${date.format}")
     private String dateFormat;*/
 
@@ -56,11 +78,12 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/controlPanel")
-    public String controlPanel() {
-        return "controlPanel";
+    public ModelAndView controlPanel(@RequestParam(value = "size", defaultValue = "1", required = false) Integer size) {
+        Page<User> users = userRepository.findAll(new PageRequest(size - 1, 1));
+        return new ModelAndView("controlPanel", "users", users);
     }
 
-    @RequestMapping(value = "/users/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+/*    @RequestMapping(value = "/users/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     TableSettings getListOfUsers() {
         List<User> users = userRepository.findAll();
@@ -74,7 +97,7 @@ public class HomeController {
         });
 
         return tableSettings;
-    }
+    }*/
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public @ResponseBody
@@ -104,10 +127,51 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String homePage(HttpServletRequest request, ModelMap model) {
+/*        Company company = companyRepository.findOne(1L);
+        company.getBranches().remove(0);
+        companyRepository.save(company);*/
+
         if (request.isUserInRole("ADMIN")) {
             model.put("users", userRepository.findAll());
         } else if (request.isUserInRole("USER")) {
             model.put("users", userRepository.findByUsernameLike("a%"));
+        }
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request1 = new HttpGet(
+                "http://affiliate.finzoom.ro/default.aspx?u_=demo&c_=ro-RO&id_=ml-sr-xml&SP_LoanPurpose=1&SP_Currency=RON&SP_LoanAmount=40000&SP_PropertyValue=140000&SP_LoanTerm=180&SP_MLLoanType=1");
+
+        try {
+            HttpResponse response = client.execute(request1);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(response.getEntity().getContent());
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("row");
+
+            final int NODE_LIST_LENGTH = nodeList.getLength();
+            for (int i = 0; i < NODE_LIST_LENGTH; i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    NodeList tempList = ((Element) node).getElementsByTagName("val");
+
+                    final int LENGTH = tempList.getLength();
+                    for (int j = 0; j < LENGTH; j++) {
+                        Element element = (Element) tempList.item(j);
+
+                        System.out.println("Attribute: " + element.getAttribute("col"));
+                        System.out.println("Text: " + element.getTextContent());
+
+                        //TODO проверка кой таг е
+                    }
+
+                    System.out.println("\n\n");
+                }
+            }
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            logger.error("Exception", e);
         }
 
         return "index";
--- src/main/webapp/WEB-INF/pages/controlPanel.jsp
+++ src/main/webapp/WEB-INF/pages/controlPanel.jsp
@@ -1,3 +1,4 @@
+<jsp:useBean id="users" scope="request" type="org.springframework.data.domain.Page"/>
 <%--
   Created by IntelliJ IDEA.
   User: imishev
@@ -20,7 +21,7 @@
         <script src="http://jqueryrock.googlecode.com/svn/trunk/jqgrid/js/jquery.jqGrid.min.js" type="text/javascript"></script>
         <script>
             $(function () {
-                $("#list").jqGrid({
+                /*$("#list").jqGrid({
                     url: '<c:url value="/users/list" />',
                     datatype: "json",
                     mtype: "GET",
@@ -38,7 +39,7 @@
                     width: 'auto',
                     loadonce: true,
                     caption: "Simple first grid"
-                });
+                });*/
 
                 /*$.ajax({
                     url: '<c:url value="/users/list" />',
@@ -59,6 +60,17 @@
         <security:authorize access="hasAnyRole('ADMIN', 'USER')">
             <label>Hidden</label>
         </security:authorize>--%>
+        <div>${users.totalPages}</div>
+        <c:set value="${users.number}" var="page" />
+        <c:if test="${page - 1 >= 0}">
+            <a href="<c:url value="/controlPanel?size=${page}" />">Previous</a>
+        </c:if>
+        <c:if test="${page + 2 <= users.totalPages}">
+            <a href="<c:url value="/controlPanel?size=${page + 2}" />">Next</a>
+        </c:if>
+        <c:forEach items="${users.content}" var="user">
+            <div>${user.username}</div>
+        </c:forEach>
         <table id="list">
             <tr>
                 <td></td>
